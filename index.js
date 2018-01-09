afb849476604024690d7ee7b87652fbb5d8d1ee3
xianjiezh <904292766@qq.com>
2018-01-09 20:25:04 +0800

    添加了callback函数

--- index.js
+++ index.js
@@ -3,38 +3,62 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
+  var pathWithQuery = request.url
   var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
-
-  if(path === '/'){
-    response.statusCode = 200
+  if (path === '/') {
+    var string = fs.readFileSync('./index.html', 'utf-8')
+    var amount = fs.readFileSync('./db', 'utf-8')
+    string = string.replace('&&&amount&&&', amount)
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write('哈哈哈')
+    response.write(string)
     response.end()
-  }else{
+  } else if(path == '/style.css'){
+    response.setHeader('Content-Type','text/css;charset=utf-8')
+    response.write('body{color:green;}')
+    response.end()
+  }else if (path === '/pay') {
+    var amount = fs.readFileSync('./db', 'utf-8')
+    
+    var newAmount = amount - 1
+    fs.writeFileSync('/db',newAmount)
+    console.log('newAmount',typeof newAmount,newAmount)
+    response.setHeader('Content-Type','text/javacript;charset=utf-8')
+    response.write(`
+      ${query.callbackName}.call(undefined,'success')
+    `)
+    response.end()
+  } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write('呜呜呜')
+    response.write('404了')
     response.end()
   }
 
   /******** 代码结束，下面不要看 ************/
 })
-
+/*
+var newAmount = amount - 1
+    fs.writeFileSynk('./db', newAmount)
+    
+    response.statusCode = 200
+    response.write(`
+      ${query.callbackName}.call(undefined,'success')
+    `)
+    
+*/
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
